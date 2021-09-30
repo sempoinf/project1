1da17daebe222bac3bcf3840dda64d3bbab9ffc5
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-09-30 12:37:34 +0300

    Create var4dz.c

---
+++ var4dz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(void)
+{
+    int c1 = 0;
+    int c2 = 0;
+    char s1;
+    
+   
+    
+    char s2[10];
+
+    scanf("%d", &c1);
+    char d;
+    while (((d = getchar()) == ' ') || (d == '|'));
+
+    int count = 0;
+    s2[count++] = d;
+    while ((d = getchar()) != '\n')
+        s2[count++] = d;
+    double c3 = 0.0;
+
+    c3 += c1;
+
+    for (int i = 0; i < count; i++)
+       c3 += (s2[i] - '0') / pow(10, i + 1);
+
+    printf("%lf\n", c3);
+
+    
+
+  return 0;
+}
